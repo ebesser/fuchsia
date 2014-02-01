4d5990d9af8c606ac9f89308476bff3b4548c5d7
kinzin <yniznik@gmail.com>
2014-02-01 12:03:22 -0500

    rails app is born

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vitriol::Application.config.secret_token = 'e8522a3488e1e37d44f7b058a5f36148673e6035b19e4801cba9daf688dae2481ad72797da9830fe1bc1ffefa8c67fa62f55296b26749ea700d84a191f574b09'
