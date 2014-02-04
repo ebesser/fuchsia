96a49cb542ab2b2743e253222b5f48ca2f2b6934
kinzin <yniznik@gmail.com>
2014-02-04 18:31:00 -0500

    sign up

--- db/migrate/20140201173729_devise_create_users.rb
+++ db/migrate/20140201173729_devise_create_users.rb
@@ -3,7 +3,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
-      t.string :username,              :null => false, :default => ""
+      t.string :username,           :null => false, :default => ""
 
       t.string :encrypted_password, :null => false, :default => ""
       t.string :img_url, :default => "http://foomandoonian.files.wordpress.com/2009/01/penguin-cartoon.png"
