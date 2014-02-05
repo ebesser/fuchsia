8d7649dbb9dca124c827d704f063f2c12e66b7a2
kinzin <yniznik@gmail.com>
2014-02-05 12:44:10 -0500

    reset password work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,8 @@ Vitriol::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
+  config.action_mailer.default_url_options = { :host => 'vitriol.herokuapp.com' }
+
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
