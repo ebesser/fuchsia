616a8d750e5aa0896129a166cb18a359067d3371
kinzin <yniznik@gmail.com>
2014-02-05 17:40:57 -0500

    Can display title, link, and description of topic in header

---
+++ app/assets/javascripts/fight_page.js
@@ -0,0 +1,19 @@
+$(function(){
+  var theTopic = $('#the_topic');
+  var theLink = $('#the_link');
+  var theBody = $('#the_description');
+  var topic_id = window.location.pathname.replace("/topics/", "")
+
+  $.ajax({
+    url: "/topics/" + topic_id,
+    type: "get",
+    dataType: "json",
+    success: (function(data){
+        console.log(data);
+        theTopic.text(data.title);
+        theLink.text(data.link);
+        theBody.text(data.body);
+        console.log(data.title + " " + data.link + " " + data.body);
+    })
+  })
+})
