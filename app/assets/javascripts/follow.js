1d2f21d5ce7009c774b9c82495aa955929f21210
kinzin <yniznik@gmail.com>
2014-02-06 15:04:03 -0500

    links added to user panel

--- app/assets/javascripts/follow.js
+++ app/assets/javascripts/follow.js
@@ -1,21 +1,22 @@
+$(function(){
+  if (window.user_id) {
+    var user_id = window.user_id;
 
-
-  $(function(){
-    if (window.user_id) {
-      var user_id = window.user_id;
-
-      $.ajax({
-        url: "/users/" + user_id + "/following",
-        type: "put",
-        dataType: "json",
-        data: {user_id: window.user_id},
-        success: (function(data){
-          for(var i in data){
-            $('#followed_topics').append( $("<li>", {class: "followed_topic"}).append(data[i]) );
-          }
-          
-        })
+    $.ajax({
+      url: "/users/" + user_id + "/following",
+      type: "put",
+      dataType: "json",
+      data: {user_id: window.user_id},
+      success: (function(data){
+        for (i in data) {
+          var new_li =  $("<li>", {class: "followed_topic"});
+          new_li.append("<a href='/topics/" + data[i]['topic_id'] + "'>" + data[i]['topic_name'] + "</a>");
+        
+          $('.home').append(new_li);
+        }             
       })
-    }
-  })
+    })
+  }
+})
   
+
